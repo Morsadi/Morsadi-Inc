58c49da72c0146aa788815bac8370cf51f5a7e65
Morsadi <badreddine.morsadi@github.com>
2019-09-04 13:46:46 -0700

    Build

---
+++ build/precache-manifest.8ecc0cca01df5307742cd322498c1c7c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e025de61a37362fa644004dfedcf0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4bdc2ed20a67d045af0",
+    "url": "/static/css/main.80373a93.chunk.css"
+  },
+  {
+    "revision": "b04c8242b0370410ecbd",
+    "url": "/static/js/2.c70a3b34.chunk.js"
+  },
+  {
+    "revision": "c4bdc2ed20a67d045af0",
+    "url": "/static/js/main.248935a4.chunk.js"
+  },
+  {
+    "revision": "c52f246a2adc53371c2e",
+    "url": "/static/js/runtime~main.e93a1498.js"
+  },
+  {
+    "revision": "8809f27a1d722293cad405aafa21066f",
+    "url": "/static/media/slide 3 hover.8809f27a.png"
+  },
+  {
+    "revision": "0ed4e8817c1c93d4ec23bad43f15e2e8",
+    "url": "/static/media/slide 3.0ed4e881.png"
+  },
+  {
+    "revision": "a0c4cdab4a539f03267f3da53c1644dc",
+    "url": "/static/media/slide1 hover.a0c4cdab.png"
+  },
+  {
+    "revision": "12af33548e6148927d4c7a42e711067e",
+    "url": "/static/media/slide1.12af3354.png"
+  },
+  {
+    "revision": "5b26ba1e86d29470ef2ace500c9c12b4",
+    "url": "/static/media/slide2 hover.5b26ba1e.png"
+  },
+  {
+    "revision": "a0485e9b3fbe9556791ecb5e70c237ba",
+    "url": "/static/media/slide2.a0485e9b.png"
+  },
+  {
+    "revision": "0c1fbf28eaf0f4c4bbc63e81671fc75f",
+    "url": "/static/media/slide4 hover.0c1fbf28.png"
+  },
+  {
+    "revision": "25bccb1a6e7b84aecdd31c2b6a626a00",
+    "url": "/static/media/slide4.25bccb1a.png"
+  }
+]);
