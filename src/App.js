78d19bae45b301b2b317f11fdb40dc7fa8e8406d
Morsadi <badreddine.morsadi@github.com>
2019-09-09 12:41:26 -0700

    Redirects

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      underConstruction: true,
+      underConstruction: false,
       isActive: 'none',
       style: {
         mainLogo: 0,
