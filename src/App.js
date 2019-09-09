377046c3e8316bcda9fd8734e61cbaa4356110aa
Morsadi <badreddine.morsadi@github.com>
2019-09-09 13:04:29 -0700

    Construction

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      underConstruction: false,
+      underConstruction: true,
       isActive: 'none',
       style: {
         mainLogo: 0,
