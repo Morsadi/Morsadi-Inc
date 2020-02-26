6be36a5fb9c303325393ab1d008f0cadaba70c28
Morsadi <badreddine.morsadi@github.com>
2020-02-25 20:13:31 -0700

    Reducing Chaining

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './assets/fonts/Roboto-Thin.ttf';
+import './assets/fonts/Roboto-Light.ttf';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
